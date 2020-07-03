18b4bb97c23fe6f8d7662be4f299da89af9b5828
alvinmr <maulanaalvin1@gmail.com>
2020-07-03 23:17:10 +0800

    selesai module regis petugas

---
+++ modules/regispetugas_module.php
@@ -0,0 +1,34 @@
+<?php
+
+include '../db/config.php';
+
+switch ($_GET['aksi']) {
+    case 'tambah':
+        $nama_petugas = $_POST['nama_petugas'];
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        mysqli_query($koneksi, "INSERT INTO `tb_petugas` VALUES (
+            null,
+            '$nama_petugas',
+            '$username',
+            '$password',
+            'petugas'
+        )") or die(mysqli_error($koneksi));
+        header('location:../index?page=regis_petugas');
+        break;
+
+    case 'edit':
+        # code...
+        break;
+
+    case 'hapus':
+        $id = $_GET['id'];
+        mysqli_query($koneksi, "DELETE FROM `tb_petugas` WHERE id_petugas = $id");
+        header('location:../index?page=regis_petugas');
+        break;
+
+    default:
+        header('location:../index.php');
+        break;
+}
